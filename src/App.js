fb303fdd80d70a68007c8dbafec04d48dba3b8d4
francasanas1 <103201464+francasanas1@users.noreply.github.com>
2022-08-23 11:27:08 -0300

    Desafio JSX & transpiling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="nav-bar">
+      <h2>Shop</h2>
+      <div id='articulos'>
+        <button>Iphone</button>
+        <button>Ipad</button>
+        <button>Mac</button>
+      </div>
+      <button id='login'>Log In</button>
+
+    </header>
+
   );
 }
 
